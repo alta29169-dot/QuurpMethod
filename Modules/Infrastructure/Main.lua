8c3a61d2adc8ea1cd2491d1aa8f9924ebbefded3
alta29169-dot <alta29169@gmail.com>
2026-08-26 20:56:50 +0800

    Update Main.lua

--- Modules/Infrastructure/Main.lua
+++ Modules/Infrastructure/Main.lua
@@ -1,5 +1,5 @@
 -- Main.lua – qurp v3 (Phase 1)
--- I love you Ashy
+-- I love you Ashley
 
 local Players = game:GetService("Players")
 local player = Players.LocalPlayer
@@ -28,6 +28,8 @@ local function heartbeat()
             Debug.info("Main", "Waiting for character...")
             continue
         end
+
+        EnemyManager.update()  -- Scans for enemies and prints if debug is on
         
         -- Update our plane status
         local myBomber = BomberManager.updatePlaneState()
